d8f722b1e4b9c8e5f064da4aec6b71425680d049
Link <602344037@qq.com>
2021-10-28 22:07:27 +0800

    educa project

---
+++ educa/courses/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Subject, Course, Module
+
+
+# Register your models here.
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+# 管理界面可以在同一页面上与父模型编辑模型
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ModuleInline]
